457bbd5115e0287977ee19b333c4be3957a1aa67
paukerspinner <xuancanhpham97@gmail.com>
2024-02-18 16:47:37 +0700

    feat: verify contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
+import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  networks: {
+    bsctest: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      accounts: [
+        process.env.PRK1 as string,
+      ]
+    }
+  }
 };
 
 export default config;
